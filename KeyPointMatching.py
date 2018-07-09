5a11063bc11e18bbd03bb81cb0a7cf591eaaa27b
MRChou <imchou239@gmail.com>
2018-07-09 19:27:47 +0800

    finished first prototype of KeypointMatching

--- KeyPointMatching.py
+++ KeyPointMatching.py
@@ -11,6 +11,7 @@ import numpy
 import cv2
 from matplotlib import pyplot as plt
 
+TEMPLATE = cv2.imread('FindlandEmblem.jpg', cv2.IMREAD_GRAYSCALE)
 RECOGNIZER = cv2.imread('Recognizer.jpg', cv2.IMREAD_GRAYSCALE)
 
 
@@ -42,8 +43,9 @@ def get_angle_and_ratio(vec1, vec2):
     return numpy.arcsin(dot), l1/l2
 
 
-def rot_and_resize(img):
-    """Rotate and resize the image according to RECOGNIZER"""
+def find_matches(img, recognizer):
+    """Find matches between img and recognizer"""
+    target = crop_topleft_coner(img)
 
     # Initiate STAR detector
     orb = cv2.ORB_create()
@@ -52,33 +54,43 @@ def rot_and_resize(img):
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
     # find the keypoints with ORB
-    kp_rec, des_rec = orb.detectAndCompute(RECOGNIZER, None)
-    kp_img, des_img = orb.detectAndCompute(img, None)
+    kp_rec, des_rec = orb.detectAndCompute(recognizer, None)
+    kp_img, des_img = orb.detectAndCompute(target, None)
 
     # find matches by brute force, sort them in the order of their distance.
     matches = bf.match(des_rec, des_img)
     matches.sort(key=lambda x: x.distance)
 
+    return kp_rec, kp_img, matches
+
+
+def rot_and_resize(img, angle, ratio):
+    """Return rotated and resized image according by angle and ratio"""
+    h = img.shape[0]
+    w = img.shape[1]
+    center = (w/2, h/2)
+    mat = cv2.getRotationMatrix2D(center, angle, ratio)
+    return cv2.warpAffine(img, mat, (w, h))
+
+
+def cal_score(img, recognizer=RECOGNIZER):
+    """Caculate target image score based on recognizer"""
+
+    # get key points and the matches
+    kp_rec, kp_img, matches = find_matches(img, recognizer=recognizer)
+
     # get vectors from 2 best matches
     vec_rec, vec_img = get_vectors_from_matches(kp_rec, kp_img, matches)
+    angle, ratio = get_angle_and_ratio(vec_rec, vec_img)  # ! the order of para
     print(vec_rec, vec_img)
-    print(get_angle_and_ratio(vec_rec, vec_img))
+    print(angle, ratio)
 
-    # Draw first 10 matches.
-    img = cv2.drawMatches(RECOGNIZER, kp_rec,
-                          img, kp_img,
-                          matches[:5],
-                          None,
-                          flags=2
-                          )
-
-    plt.imshow(img)
+    result = rot_and_resize(img, 180*angle/numpy.pi, ratio)
+    plt.imshow(result, cmap='gray')
     plt.show()
 
 
 if __name__ == '__main__':
-    TRAIN = cv2.imread('FindlandEmblem_Brushed_Rot36.jpg', cv2.IMREAD_GRAYSCALE)
-    TRAIN = crop_topleft_coner(TRAIN)
-    rot_and_resize(TRAIN)
-
+    TRAIN = cv2.imread('FindlandEmblem_Brushed_Rot18.jpg', cv2.IMREAD_GRAYSCALE)
+    cal_score(TRAIN)
 
